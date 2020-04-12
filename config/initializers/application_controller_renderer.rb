39e6f254514384c700b75e9ff8385ce1e95ef90e
Tatsuro <tatsurow1122@gmail.com>
2020-04-12 16:25:14 +0900

    [Update] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '99124e9aded79b4ed5e770631e61e61444f24683d515a2888661bf40c2d51b31a3f28f05e897f0ca7a3797e8f38241e35e1e8fa18c2ab3b58fda9cd40744a6c5'
